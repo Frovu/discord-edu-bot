a7034e1f9c7f559bef8eaa2a9bddd32f8384a772
Frovy <sfrovis@gmail.com>
2020-04-07 22:41:55 +0300

    initial 2

---
+++ functions/logs.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = './logs/';
+
+if (!fs.existsSync(path)){
+    fs.mkdirSync(path);
+    console.log('Created log directory.');
+}
+
+function getFileName(date, msg=false){
+	if(msg)
+		return `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}.msgs`;
+	return `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}.log`;
+}
+
+let logStream = fs.createWriteStream((path+getFileName(new Date())), {flags:'a'});
+
+function logInFile(date, msg, topic){
+    // create new log file if needed
+	if(!logStream.path.endsWith(getFileName(date))) {
+        createLogStreams(new Date());
+		module.exports('BOT', `Creating new logInFile: ${getFileName(date)}`);
+	}
+    logStream.write(msg + '\n');
+}
+
+function Pad (num, size) {
+    return ('         ' + num).substr(-size);
+}
+
+function THeader (date) {
+	return `[${Pad(date.getHours(),2)}:${Pad(date.getMinutes(),2)}]`;
+}
+
+// Date stuff
+function DHeader (date) {
+	return `[${Pad(date.getDate(),2)}/${Pad(date.getMonth()+1,2)}/${Pad(date.getFullYear(),2)}]`;
+}
+
+module.exports = function (topic, message) {
+    const date = new Date();
+    msg = `[${topic.padEnd(5)}]${DHeader(date)}${THeader(date)} ${message}`;
+
+    // Loging to file
+    logInFile(date, msg, topic)
+    console.log(msg);
+};
---
+++ main.js
@@ -0,0 +1,49 @@
+
+const tokens = require('./tokens.json');
+const Discord = require('discord.js');
+// log for use in other modules too
+log = require('./functions/logs.js');
+
+const commands = require('./functions/cmds.js');
+
+// unhandled rejection
+process.on('unhandledRejection', (reason) => {
+	log(`ERROR_UNHANDLED!`, reason.stack);
+	client.users.fetch('236931374722973698').then(dm=>{dm.send(`\`\`\`\n${reason.stack}\`\`\``)});	// ping Frovy
+});
+
+// If start up went through correctly, inform about it.
+log('BOT', `Starting edu bot.\n  Node version: ${process.version}\n  Discord.js version: ${Discord.version}`);
+
+// Discord bot client. The bot itself.
+// should be global for use in other modules.
+client = new Discord.Client({forceFetchUsers: true});
+// Connection to Discord API.
+client.login(tokens.discordbot);
+
+// When the bot recieved connection.
+client.on("ready", function () {
+	log(`BOT`, `The bot is online!`);
+	// set presence
+	client.user.setActivity("distance learning", {type: "PLAYING"});
+});
+
+// Bot disconnection.
+client.on("disconnect", function () {
+	log(`BOT`, `Client disconnected.`);
+});
+
+client.on("error", async (err) => {
+	log(`ERROR_EVENT`, `Client error: ${err}`);
+});
+
+// Message responses
+client.on("message", async message => {
+	try {
+		// Ignoring other bots.
+		if(message.author.bot) return;
+		await commands.process(message);
+	} catch(e) {
+		log(`ERROR`, `Failed on.message: ${e.stack}`);
+	}
+});
