a470337fcf9ab3b8ed06592157ad5b184272c085
Frovy <sfrovis@gmail.com>
2020-05-01 16:09:43 +0300

    fix: .getlog q

--- commands/system/getlog.js
+++ commands/system/getlog.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 
 // send last n lines
-async function lastLog(logFiles, author, a=false) {
-    let filename = logFiles[logFiles.length-2+a];
+async function lastLog(logFiles, author) {
+    let filename = logFiles[logFiles.length-1];
     let text = fs.readFileSync('./logs/'+filename).toString();
     if(text.split('\n').length < 50 && logFiles.length>2) {
         // read one more file
-        filename = logFiles[logFiles.length-4+a];
+        filename = logFiles[logFiles.length-2];
         text = fs.readFileSync('./logs/'+filename)+'\n'+text;
     }
     text = '```'+text.split('\n').slice(-50).join('\n')+'```';
